0aa8aeb7242c191e038d77ccdd7a287ed3f4b6f2
sravyavvs <45485485+sravyavvs@users.noreply.github.com>
2022-07-31 17:10:21 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,280 @@
+package assignment;
+
+public class Q1 {
+	
+
+	public static void main(String[] args) {
+		int i,len=7;
+		 String name="INEURON";
+
+		
+		  for(i=0;i<len;i++)
+		  {
+		    if(name.charAt(i)=='A'|| name.charAt(i)=='B' || name.charAt(i)=='C'|| name.charAt(i)=='E'|| name.charAt(i)=='F' || name.charAt(i)=='G'|| name.charAt(i)=='I'|| name.charAt(i)=='J' || name.charAt(i)=='O'|| name.charAt(i)=='P'|| name.charAt(i)=='Q' || name.charAt(i)=='R'|| name.charAt(i)=='S'|| name.charAt(i)=='T'|| name.charAt(i)=='Z')
+		    {
+		      System.out.print("   *****");
+		    }
+		    else if(name.charAt(i)=='H' || name.charAt(i)=='K'|| name.charAt(i)=='M' || name.charAt(i)=='N' || name.charAt(i)=='U'|| name.charAt(i)=='V'|| name.charAt(i)=='W'|| name.charAt(i)=='X'|| name.charAt(i)=='Y')
+		    {
+		      System.out.print("   *   *");
+		    }
+		    else if(name.charAt(i)=='D')
+		    {
+		      System.out.print("       *");
+		    }
+		    else if(name.charAt(i)=='L')
+		    {
+		      System.out.print("   *    ");
+		    }
+		  }
+		  System.out.print("\n");
+		  for(i=0;i<len;i++)
+		  {
+		    if(name.charAt(i)=='A'|| name.charAt(i)=='B' || name.charAt(i)=='G'|| name.charAt(i)=='H'|| name.charAt(i)=='O' || name.charAt(i)=='P'|| name.charAt(i)=='Q'|| name.charAt(i)=='R' || name.charAt(i)=='U'|| name.charAt(i)=='V'|| name.charAt(i)=='W')
+		    {
+		      System.out.print("   *   *");
+		    }
+		    else if(name.charAt(i)=='C'|| name.charAt(i)=='E' || name.charAt(i)=='F'|| name.charAt(i)=='L'|| name.charAt(i)=='S')
+		    {
+		      System.out.print("   *    ");
+		    }
+		    if(name.charAt(i)=='M')
+		    {
+		      System.out.print("   ** **");
+		    }
+		    else if(name.charAt(i)=='Y')
+		    {
+		      System.out.print("    * * ");
+		    }
+		    else if(name.charAt(i)=='K')
+		    {
+		      System.out.print("   *  * ");
+		    }
+		    else if(name.charAt(i)=='I'||name.charAt(i)=='J'||name.charAt(i)=='T')
+		    {
+		      System.out.print("     *  ");
+		    }
+		    else if(name.charAt(i)=='D')
+		    {
+		      System.out.print("       *");
+		    }
+		    else if(name.charAt(i)=='N')
+		    {
+		      System.out.print("   **  *");
+		    }
+		    else if(name.charAt(i)=='Z')
+		    {
+		      System.out.print("      * ");
+		    }
+		    else if(name.charAt(i)=='X')
+		    {
+		      System.out.print("        ");
+		    }
+		  }
+		  System.out.print("\n");
+		  for(i=0;i<len;i++)
+		  {
+		    if(name.charAt(i)=='A'|| name.charAt(i)=='B' || name.charAt(i)=='G'|| name.charAt(i)=='H'||name.charAt(i)=='N'|| name.charAt(i)=='O' || name.charAt(i)=='P'|| name.charAt(i)=='Q'|| name.charAt(i)=='R' || name.charAt(i)=='U'|| name.charAt(i)=='V'|| name.charAt(i)=='W')
+		    {
+		      System.out.print("   *   *");
+		    }
+		    else if(name.charAt(i)=='C'|| name.charAt(i)=='E' || name.charAt(i)=='F'|| name.charAt(i)=='L'|| name.charAt(i)=='S')
+		    {
+		      System.out.print("   *    ");
+		    }
+		    if(name.charAt(i)=='M')
+		    {
+		      System.out.print("   * * *");
+		    }
+		    else if(name.charAt(i)=='Y')
+		    {
+		      System.out.print("     *  ");
+		    }
+		    else if(name.charAt(i)=='K')
+		    {
+		      System.out.print("   * *  ");
+		    }
+		    else if(name.charAt(i)=='I'||name.charAt(i)=='J'||name.charAt(i)=='T')
+		    {
+		      System.out.print("     *  ");
+		    }
+		    else if(name.charAt(i)=='D')
+		    {
+		      System.out.print("       *");
+		    }
+		    else if(name.charAt(i)=='X')
+		    {
+		      System.out.print("    * * ");
+		    }
+		    else if(name.charAt(i)=='Z')
+		    {
+		      System.out.print("        ");
+		    }
+		  }
+		  System.out.print("\n");
+		  for(i=0;i<len;i++)
+		  {
+		    if(name.charAt(i)=='A'|| name.charAt(i)=='B'|| name.charAt(i)=='D'|| name.charAt(i)=='E' || name.charAt(i)=='F'|| name.charAt(i)=='G'|| name.charAt(i)=='H'|| name.charAt(i)=='P'|| name.charAt(i)=='Q'|| name.charAt(i)=='R'|| name.charAt(i)=='S' )
+		    {
+		      System.out.print("   *****");
+		    }
+		    else if(name.charAt(i)=='C')
+		    {
+		      System.out.print("   *    ");
+		    }
+		    if(name.charAt(i)=='M'|| name.charAt(i)=='O'|| name.charAt(i)=='V'|| name.charAt(i)=='W'|| name.charAt(i)=='U')
+		    {
+		      System.out.print("   *   *");
+		    }
+		    else if(name.charAt(i)=='Z')
+		    {
+		      System.out.print("     *  ");
+		    }
+		    else if(name.charAt(i)=='K')
+		    {
+		      System.out.print("   **   ");
+		    }
+		    else if(name.charAt(i)=='I'||name.charAt(i)=='J'||name.charAt(i)=='T'|| name.charAt(i)=='Y')
+		    {
+		      System.out.print("     *  ");
+		    }
+		    else if(name.charAt(i)=='L')
+		    {
+		      System.out.print("   *    ");
+		    }
+		    else if(name.charAt(i)=='N')
+		    {
+		      System.out.print("   * * *");
+		    }
+		    else if(name.charAt(i)=='X')
+		    {
+		      System.out.print("     *  ");
+		    }
+		  }
+		  System.out.print("\n");
+		  for(i=0;i<len;i++)
+		  {
+		    if(name.charAt(i)=='A'|| name.charAt(i)=='B'|| name.charAt(i)=='D'|| name.charAt(i)=='H' || name.charAt(i)=='M'|| name.charAt(i)=='N'|| name.charAt(i)=='O'||name.charAt(i)=='U'|| name.charAt(i)=='V')
+		    {
+		      System.out.print("   *   *");
+		    }
+		    else if(name.charAt(i)=='C'|| name.charAt(i)=='E'|| name.charAt(i)=='F'|| name.charAt(i)=='L'|| name.charAt(i)=='P')
+		    {
+		      System.out.print("   *    ");
+		    }
+		    if(name.charAt(i)=='G'|| name.charAt(i)=='Q'|| name.charAt(i)=='S')
+		    {
+		      System.out.print("       *");
+		    }
+		    else if(name.charAt(i)=='K')
+		    {
+		      System.out.print("   * *  ");
+		    }
+		    else if(name.charAt(i)=='I'||name.charAt(i)=='J'||name.charAt(i)=='T'|| name.charAt(i)=='Y')
+		    {
+		      System.out.print("     *  ");
+		    }
+		    else if(name.charAt(i)=='R')
+		    {
+		      System.out.print("   * *  ");
+		    }
+		    else if(name.charAt(i)=='W')
+		    {
+		      System.out.print("   * * *");
+		    }
+		    else if(name.charAt(i)=='X')
+		    {
+		      System.out.print("    * * ");
+		    }
+		    else if(name.charAt(i)=='Z')
+		    {
+		      System.out.print("        ");
+		    }
+		  }
+		  System.out.print("\n");
+		  for(i=0;i<len;i++)
+		  {
+		    if(name.charAt(i)=='A'|| name.charAt(i)=='B'|| name.charAt(i)=='D'|| name.charAt(i)=='H' || name.charAt(i)=='M'|| name.charAt(i)=='O'||name.charAt(i)=='U'|| name.charAt(i)=='V')
+		    {
+		      System.out.print("   *   *");
+		    }
+		    else if(name.charAt(i)=='C'|| name.charAt(i)=='E'|| name.charAt(i)=='F'|| name.charAt(i)=='L'|| name.charAt(i)=='P')
+		    {
+		      System.out.print("   *    ");
+		    }
+		    if(name.charAt(i)=='G'|| name.charAt(i)=='Q'|| name.charAt(i)=='S')
+		    {
+		      System.out.print("       *");
+		    }
+		    else if(name.charAt(i)=='K')
+		    {
+		      System.out.print("   *  * ");
+		    }
+		    else if(name.charAt(i)=='I'||name.charAt(i)=='T'|| name.charAt(i)=='Y')
+		    {
+		      System.out.print("     *  ");
+		    }
+		    else if(name.charAt(i)=='R')
+		    {
+		      System.out.print("   *  * ");
+		    }
+		    else if(name.charAt(i)=='W')
+		    {
+		      System.out.print("   ** **");
+		    }
+		    else if(name.charAt(i)=='J')
+		    {
+		      System.out.print("   * *  ");
+		    }
+		    else if(name.charAt(i)=='N')
+		    {
+		      System.out.print("   *  **");
+		    }
+		    else if(name.charAt(i)=='Z')
+		    {
+		      System.out.print("    *    ");
+		    }
+		    else if(name.charAt(i)=='X')
+		    {
+		      System.out.print("        ");
+		    }
+		  }
+		  System.out.print("\n");
+		  for(i=0;i<len;i++)
+		  {
+		    if(name.charAt(i)=='B'|| name.charAt(i)=='C'|| name.charAt(i)=='D'|| name.charAt(i)=='E' || name.charAt(i)=='G'|| name.charAt(i)=='I'|| name.charAt(i)=='O'||name.charAt(i)=='L'|| name.charAt(i)=='S'|| name.charAt(i)=='U'|| name.charAt(i)=='Z')
+		    {
+		      System.out.print("   *****");
+		    }
+		    else if(name.charAt(i)=='A'|| name.charAt(i)=='H'|| name.charAt(i)=='M'|| name.charAt(i)=='N')
+		    {
+		      System.out.print("   *   *");
+		    }
+		    if(name.charAt(i)=='F'|| name.charAt(i)=='P')
+		    {
+		      System.out.print("   *    ");
+		    }
+		    else if(name.charAt(i)=='J')
+		    {
+		      System.out.print("   ***  ");
+		    }
+		    else if(name.charAt(i)=='K'||name.charAt(i)=='R')
+		    {
+		      System.out.print("   *   *");
+		    }
+		    else if(name.charAt(i)=='Q')
+		    {
+		      System.out.print("       *");
+		    }
+		    else if(name.charAt(i)=='T'|| name.charAt(i)=='V'|| name.charAt(i)=='Y')
+		    {
+		      System.out.print("     *  ");
+		    }
+		    else if(name.charAt(i)=='W'||name.charAt(i)=='X')
+		    {
+		      System.out.print("   *   *");
+		    }
+		  }
+		  
+		}
+}
